f759efc4755e385be260f18572a3f7f8497dac99
ST10377344 <137050962+10377344@users.noreply.github.com>
2025-08-22 10:40:40 +0200

    navbar added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,4 +63,5 @@ dependencies {
     implementation("de.hdodenhof:circleimageview:3.1.0")
     implementation("com.github.bumptech.glide:glide:4.12.0")
     kapt("com.github.bumptech.glide:compiler:4.12.0")
+    implementation ("com.google.android.material:material:1.11.0")
 }
--- app/src/main/java/vcmsa/projects/wil_hustlehub/MainActivity.kt
+++ app/src/main/java/vcmsa/projects/wil_hustlehub/MainActivity.kt
@@ -1,13 +1,58 @@
 package vcmsa.projects.wil_hustlehub
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+
+        if (savedInstanceState == null) {
+            openFragment(HomeFragment())
+        }
+
+
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    openFragment(HomeFragment())
+                    true
+                }
+                R.id.nav_browse -> {
+                    openFragment(BrowseServicesFragment())
+                    true
+                }
+                R.id.nav_offer_skills -> {
+                    openFragment(OfferSkillsFragment())
+                    true
+                }
+                R.id.nav_profile -> {
+                    openFragment(ProfileFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun openFragment(fragment: androidx.fragment.app.Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.nav_host_fragment, fragment)
+            .commit()
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        // 👇 Hide nav bar for Login & Register
+        if (fragment is LoginFragment || fragment is RegisterFragment) {
+            bottomNav.visibility = View.GONE
+        } else {
+            bottomNav.visibility = View.VISIBLE
+        }
     }
 }
