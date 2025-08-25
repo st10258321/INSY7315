54f1123368a0636500668e3dda30b49aa69f6541
st10258321 <ST10258321@vcconnect.edu.za>
2025-08-25 15:34:43 +0200

    Update

---
+++ app/src/main/java/vcmsa/projects/wil_hustlehub/View/ProviderBookings.kt
@@ -0,0 +1,14 @@
+package vcmsa.projects.wil_hustlehub.View
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import vcmsa.projects.wil_hustlehub.R
+
+class ProviderBookings : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
