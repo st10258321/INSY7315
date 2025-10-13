691284a640156f06a16c7a194c1f5e06b5fd30c0
st10258321 <ST10258321@vcconnect.edu.za>
2025-10-13 21:39:31 +0200

    updated gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,6 +76,8 @@ dependencies {
     implementation("com.github.bumptech.glide:glide:4.12.0")
     kapt("com.github.bumptech.glide:compiler:4.12.0")
     implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.code.gson:gson:2.11.0")
+
 }
 
 // Detekt configuration
