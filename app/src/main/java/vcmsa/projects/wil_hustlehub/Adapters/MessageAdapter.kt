0f22b34173304fde26dcc4d6a4a00bb4ae50b4f8
ST10377344 <st10377344@vcconnect.edu.za>
2025-10-14 23:37:37 +0200

    Chat.xml items

---
+++ app/src/main/java/vcmsa/projects/wil_hustlehub/Adapters/MessageAdapter.kt
@@ -0,0 +1,71 @@
+package vcmsa.projects.wil_hustlehub.Adapters
+
+    import android.view.LayoutInflater
+    import android.view.ViewGroup
+    import androidx.recyclerview.widget.RecyclerView
+    import vcmsa.projects.wil_hustlehub.Model.Message
+    import vcmsa.projects.wil_hustlehub.databinding.ItemChatMessageReceivedBinding
+    import vcmsa.projects.wil_hustlehub.databinding.ItemChatMessageSentBinding
+
+    class MessageAdapter(
+        private var messages: MutableList<Message>,
+        private val currentUserId: String
+    ) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+        companion object {
+            private const val VIEW_TYPE_SENT = 1
+            private const val VIEW_TYPE_RECEIVED = 2
+        }
+
+        override fun getItemViewType(position: Int): Int {
+            return if (messages[position].senderId == currentUserId)
+                VIEW_TYPE_SENT
+            else
+                VIEW_TYPE_RECEIVED
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+            return if (viewType == VIEW_TYPE_SENT) {
+                val binding = ItemChatMessageSentBinding.inflate(
+                    LayoutInflater.from(parent.context), parent, false
+                )
+                SentMessageViewHolder(binding)
+            } else {
+                val binding = ItemChatMessageReceivedBinding.inflate(
+                    LayoutInflater.from(parent.context), parent, false
+                )
+                ReceivedMessageViewHolder(binding)
+            }
+        }
+
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            val msg = messages[position]
+            if (holder is SentMessageViewHolder) holder.bind(msg)
+            else if (holder is ReceivedMessageViewHolder) holder.bind(msg)
+        }
+
+        override fun getItemCount(): Int = messages.size
+
+        fun updateMessages(newMessages: List<Message>) {
+            messages.clear()
+            messages.addAll(newMessages)
+            notifyDataSetChanged()
+        }
+
+        inner class SentMessageViewHolder(private val binding: ItemChatMessageSentBinding) :
+            RecyclerView.ViewHolder(binding.root) {
+            fun bind(msg: Message) {
+                binding.messageText.text = msg.message
+                binding.messageTimestamp.text = msg.timeSent
+            }
+        }
+
+        inner class ReceivedMessageViewHolder(private val binding: ItemChatMessageReceivedBinding) :
+            RecyclerView.ViewHolder(binding.root) {
+            fun bind(msg: Message) {
+                binding.messageText.text = msg.message
+                binding.messageTimestamp.text = msg.timeSent
+            }
+        }
+    }
+
