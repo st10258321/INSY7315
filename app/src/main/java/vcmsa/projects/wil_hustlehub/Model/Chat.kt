320c18b983d8873e24673bf4c77df39b1dd49cf9
ST10377344 <st10377344@vcconnect.edu.za>
2025-10-14 14:45:39 +0200

    Chat.kt

--- app/src/main/java/vcmsa/projects/wil_hustlehub/Model/Chat.kt
+++ app/src/main/java/vcmsa/projects/wil_hustlehub/Model/Chat.kt
@@ -1,12 +1,11 @@
 package vcmsa.projects.wil_hustlehub.Model
 
-data class Chat(
-    val chatId: String = "",
-    val userId: String = "",
-    val serviceProviderId: String = "",
-    val serviceId: String = "",
-    val serviceName: String = "",
-    val lastMessage: String = "",
-    val lastMessageTime: String = "",
-    val createdDate: String = ""
-)
+
+    data class Chat(
+        val id: String,            // Unique chat ID
+        val contactName: String,   // Name of the contact
+        val lastMessage: String,   // Last message preview
+        val timestamp: Long        // Last message time
+        )
+
+
